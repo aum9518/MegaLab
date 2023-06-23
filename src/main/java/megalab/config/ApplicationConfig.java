78ac766a695d0baa805b0984ed454bcd02793388
NITRO <sanadylov@gmail.com>
2023-06-23 20:56:17 +0600

    add configs

---
+++ src/main/java/megalab/config/ApplicationConfig.java
@@ -0,0 +1,40 @@
+package megalab.config;
+
+import lombok.RequiredArgsConstructor;
+import megalab.exception.NotFoundException;
+import megalab.repository.UserInfoRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class ApplicationConfig {
+    private final UserInfoRepository userInfoRepository;
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return nickName -> userInfoRepository.getUserInfoByNickName(nickName)
+                .orElseThrow(() -> new NotFoundException("User with email: %s not found".formatted(nickName)));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(userDetailsService());
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+}
