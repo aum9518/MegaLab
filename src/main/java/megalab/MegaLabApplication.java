dba673edc7d9371d227ace65947d57e2ec465dd3
manasabduganiuulu <abduganiuullumanasbek@gmail.com>
2023-06-23 17:55:58 +0600

    first commit

---
+++ src/main/java/megalab/MegaLabApplication.java
@@ -0,0 +1,13 @@
+package megalab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MegaLabApplication.class, args);
+    }
+
+}
