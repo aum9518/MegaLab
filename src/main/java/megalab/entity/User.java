a08a67f0ebec934c425b3a7fe6606f73332698cb
manasabduganiuulu <abduganiuullumanasbek@gmail.com>
2023-06-23 18:32:20 +0600

    add UserEntity

--- src/main/java/megalab/entity/User.java
+++ src/main/java/megalab/entity/User.java
@@ -1,4 +1,23 @@
 package megalab.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE,generator = "user_gen")
+    @SequenceGenerator(name = "user_gen",sequenceName = "user_seq",allocationSize = 1)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String nickName;
+    private String image;
+    private boolean isBlock;
 }
