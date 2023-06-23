438b855f31613cce2659b2860e142d083d6f2763
NITRO <sanadylov@gmail.com>
2023-06-23 20:30:13 +0600

    add config classes

--- src/main/java/megalab/repository/UserInfoRepository.java
+++ src/main/java/megalab/repository/UserInfoRepository.java
@@ -3,5 +3,8 @@ package megalab.repository;
 import megalab.entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserInfoRepository extends JpaRepository<UserInfo, Long> {
+    Optional<UserInfo> getUserInfoByNickName(String nickName);
 }
