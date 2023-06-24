38321048afcec10c37ae1fd64e6360b9e350a76c
NITRO <sanadylov@gmail.com>
2023-06-24 12:31:11 +0600

    add methods NewsServiceImpl

--- src/main/java/megalab/repository/UserRepository.java
+++ src/main/java/megalab/repository/UserRepository.java
@@ -3,10 +3,12 @@ package megalab.repository;
 import megalab.entity.User;
 import megalab.entity.UserInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
+    @Query("SELECT u FROM User u LEFT JOIN u.userInfo uf WHERE uf.nickName = ?1")
+    Optional<User> getUserByUserInfoNickName(String nickName);
 
 }
