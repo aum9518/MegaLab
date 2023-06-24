334412ab4951e4d2a3141af01e4956e54fba8dcf
Davran2005 <davranjoldoshbaev2@gmail.com>
2023-06-24 14:19:14 +0600

    search Category

--- src/main/java/megalab/repository/CategoryRepository.java
+++ src/main/java/megalab/repository/CategoryRepository.java
@@ -7,14 +7,20 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     boolean existsByName(String name);
+
     @Query("SELECT NEW megalab.dto.category.CategoryResponse(c.id,c.name) from Category c order by c.id,c.name asc ")
     Page<CategoryResponse> getAllCategory(Pageable pageable);
 
-   Optional <CategoryResponse> getByIdCategory(Long id);
+    Optional<CategoryResponse> getByIdCategory(Long id);
+
+    @Query("SELECT NEW megalab.dto.category.CategoryResponse (c.id,c.name) FROM Category c where c.name ilike concat(:word, '%') or " +
+            "c.name ilike concat('%',:word,'%')")
+    Page<CategoryResponse> searchCategoryByName(@Param("word") String word,Pageable pageable);
 }
