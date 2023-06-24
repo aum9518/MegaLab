bb1e11670549b3bdd42b4456177929095331dda0
Davran2005 <davranjoldoshbaev2@gmail.com>
2023-06-24 19:25:47 +0600

    update comment save

--- src/main/java/megalab/service/impl/CommentServiceImpl.java
+++ src/main/java/megalab/service/impl/CommentServiceImpl.java
@@ -6,9 +6,11 @@ import megalab.dto.comment.CommentPagination;
 import megalab.dto.comment.CommentRequest;
 import megalab.dto.comment.CommentResponse;
 import megalab.entity.Comment;
+import megalab.entity.News;
 import megalab.entity.User;
 import megalab.exception.NotFoundException;
 import megalab.repository.CommentRepository;
+import megalab.repository.NewsRepository;
 import megalab.repository.UserRepository;
 import megalab.service.CommentService;
 import org.springframework.data.domain.Page;
@@ -19,23 +21,60 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CommentServiceImpl implements CommentService {
     private final CommentRepository commentRepository;
     private final UserRepository userRepository;
+    private final NewsRepository newsRepository;
 
     @Override
-    public SimpleResponse saveComment(CommentRequest commentRequest) {
+    public SimpleResponse saveComment(Long userId, Long newsId, CommentRequest commentRequest) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("User with id: " + userId + "is not fount"));
+        News news = newsRepository.findById(newsId)
+                .orElseThrow(() -> new NotFoundException("News with id: " + newsId + "is not fount"));
         Comment comment = new Comment();
         comment.setText(commentRequest.text());
         comment.setCreateDate(ZonedDateTime.now());
         comment.setUpdatedDate(ZonedDateTime.now());
+        comment.setUser(user);
+        user.setComments(List.of(comment));
+        comment.setNews(news);
+        news.setComments(List.of(comment));
         commentRepository.save(comment);
+        userRepository.save(user);
+        newsRepository.save(news);
         return SimpleResponse.builder()
                 .status(HttpStatus.ACCEPTED)
                 .message("Successfully added")
+                .build();
+    }
+
+    @Override
+    public CommentResponse save2Comment(Long comId, Long userId, Long newsId, CommentRequest commentRequest) {
+        List<Comment>comments = new ArrayList<>();
+        User user1 = userRepository.findById(userId)
+                .orElseThrow(() -> new NotFoundException("User with id: " + userId + "is not fount"));
+        News news = newsRepository.findById(newsId)
+                .orElseThrow(() -> new NotFoundException("News with id: " + newsId + "is not fount"));
+        Comment comment1 = commentRepository.findById(comId).orElseThrow(() -> new NotFoundException("Comment with id: " + comId + "is not fount"));
+        comment1.setComments(comments);
+        comment1.setText(commentRequest.text());
+        comment1.setCreateDate(ZonedDateTime.now());
+        comment1.setUpdatedDate(ZonedDateTime.now());
+        comment1.setUser(user1);
+        user1.setComments(List.of(comment1));
+        comment1.setNews(news);
+        news.setComments(List.of(comment1));
+        commentRepository.save(comment1);
+        userRepository.save(user1);
+        newsRepository.save(news);
+        return CommentResponse.builder()
+                .text(commentRequest.text())
                 .build();
     }
 
