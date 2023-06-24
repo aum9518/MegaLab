daa0225329d90a277f8aad24aed12d705ded08c2
Davran2005 <davranjoldoshbaev2@gmail.com>
2023-06-24 16:43:58 +0600

    update

--- src/main/java/megalab/api/AuthenticationApi.java
+++ src/main/java/megalab/api/AuthenticationApi.java
@@ -6,7 +6,7 @@ import megalab.dto.authentication.AuthenticationResponse;
 import megalab.dto.authentication.SignInRequest;
 import megalab.dto.authentication.SignUpRequest;
 import megalab.service.AuthenticationService;
-import org.springframework.security.access.prepost.PreAuthorize;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
